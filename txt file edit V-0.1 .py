8d85f401e344bd15061e8352cf3b7c142b18ac26
HelloBox114514 <111820410+HelloBox114514@users.noreply.github.com>
2023-02-10 22:05:18 +0800

    Update txt file edit V-0.1 .py

--- txt file edit V-0.1 .py
+++ txt file edit V-0.1 .py
@@ -1,9 +1,9 @@
 pl=input("请输入txt地址,地址必须存在且文件已创建\n提示：F:/test.txt\n")
-print("请选择模式\n[1]阅读txt\n[2]编辑\n[3]新建文本文档（第一次运行请选择此选项）")
+print("请选择模式\n[1]阅读txt\n[2]编辑\n")
 d=input()
 if d=="1":
-    with open(pl, encoding='utf-8', mode = "r") as f:  # 打开文件
-        data = f.read()  # 读取文件
+    with open(pl, encoding='utf-8', mode = "r") as f:
+        data = f.read()
         print(data)
 if d=="2":
     with open(pl, encoding='utf-8', mode = "r") as f:
